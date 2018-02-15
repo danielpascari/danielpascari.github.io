350982697b7292c1eff0f52863f348fed1522132
danielpascari <vajno.1997@mail.ru>
2018-02-15 11:47:09 +0200

    fin

---
+++ orange-addnew/mail.php
@@ -0,0 +1,13 @@
+﻿<?php
+if(isset($_POST['submit'])){
+    $name = $_POST['name'];
+    $mail = $_POST['mail'];
+    $phone = $_POST['phone'];
+    $message = $_POST['message'];
+
+    $myEmail = 'vajno.1997@gmail.com';
+    $subject = ('Mesaj de la: ' . $name . '; ' .'mail: ' . $mail . '; ' . 'numar: ' . $phone . '; ' . 'Mesaj: ');
+
+    mail($myEmail, $subject, $message);
+}
+?>
